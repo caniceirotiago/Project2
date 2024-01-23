229108a740fec2d3649b90d0ea44c409b8ee79f4
caniceiro <caniceiro@dei.uc.pt>
2024-01-23 16:03:17 +0000

    Algumas mudanças

--- homepage.js
+++ homepage.js
@@ -17,5 +17,5 @@ document.addEventListener('DOMContentLoaded', function() {
 btn-ExitSW.addEventListener("click", function() {
     localStorage.clear();
     location.href = "index.html";
-  });
+});
 
