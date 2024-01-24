6e31e66c2cca742e5af454ccda7bacf8f08c0db7
caniceiro <caniceiro@dei.uc.pt>
2024-01-24 18:07:06 +0000

    Bug Edição corrigido

--- homepage.js
+++ homepage.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('usernameDisplay').textContent = welcomeMsg+  storedUsername;
     }
     loadTasks();
+    saveTasks();
 });
 
 /* LOAD ALL TASKS */
