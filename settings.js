7cf2a94946dbc2cf7b4862256f335d4111eacd18
Ramat-afk <sofia20@earthling.net>
2024-01-24 23:49:22 +0000

    -same as before

--- settings.js
+++ settings.js
@@ -12,35 +12,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+let themeBtns = document.getElementsByClassName('btn-theme');
+
+for (let i = 0; i < themeBtns.length; i++) {
+    themeBtns[i].addEventListener('click', function() {
+        console.log('pressed' +themeBtns[i].textContent);
+    })
+} 
 
 
 
-/* THEME changing code */
-
-/* Variable switcher  now references the button
-through a CSS selector function */
-const switcher = document.querySelector('.btn-theme');
-
-/* Linking the switcher to an EventListener*/
-switcher.addEventListener('click',function() {
-    /* Logic for swapping to the currently non used theme*/
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    /* Conditional swapping of the Button's inner Text */
-    const className = document.body.className;
-    if (className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
-
-    /* message display on console: 
-    className = document.body.className; being that
-    <body class="light-theme"> */
-    console.log('current class name: ' + className);
-});
-
-
-
-
+/* THEME changing code--- reverted */
